1728d25d133cd183cb58b560d52a68398565eea0
Viraj Shetty <viraj.vls@gmail.com>
2020-04-13 00:02:41 -0400

    completed application

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# cu-hw-09-readme-generator
+
+    
+# Readme Generator
+## Description
+An application to generate a readme
+        
+## Table of Contents
+        
+* [Installation](#installation)
+        
+* [Usage](#usage)
+        
+* [License](#license)
+        
+* [Contributing](#contributing)
+        
+* [Tests](#tests)
+        
+* [Questions](#questions)
+        
+## Installation
+        
+To install necessary dependencies, run the following command:
+        
+```
+npm i, npm inquirer, npm axios, npm file-system
+```
+        
+## Usage
+Install all dependencies before running application
+        
+## License
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+        
+## Contributing
+        
+Open source and free to contribute
+## Tests
+        
+To run tests, run the following command:
+        
+```
+npm test
+```
+        
+## Questions
+![user profile image](https://avatars0.githubusercontent.com/u/41696263?v=4)
+https://www.linkedin.com/in/virajshetty/
+        
---
+++ index.js
@@ -0,0 +1,131 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Please provide project title?",
+        name: "project"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project.",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        choices: ["BSD","Apache","Eclipse"],
+        name: "licensetype"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "dependencies",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "about"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributing"
+    }
+]).then(function(answer) {
+    
+    // console.log(answer);
+
+    if (answer.licensetype === "BSD") {
+        answer.licensetype = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    }
+    if (answer.licensetype === "Apache") {
+        answer.licensetype = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    }
+    if (answer.licensetype === "Eclipse") {
+        answer.licensetype = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+    }
+    
+    const queryURL = `https://api.github.com/users/${answer.username}`;
+    
+    axios.get(queryURL).then(function(res) {
+        let userImage = res.data.avatar_url;
+        let userBlog = res.data.blog;
+
+        const data = getData(answer, userImage,userBlog);
+
+        fs.writeFile("README.md", data, function() {
+
+        });
+    })   
+});
+
+
+function getData({username, email, project, description, licensetype, dependencies, tests, about, contributing},userImage,userBlog) {
+
+    return `
+    
+# ${project}
+## Description
+${description}
+        
+## Table of Contents
+        
+* [Installation](#installation)
+        
+* [Usage](#usage)
+        
+* [License](#license)
+        
+* [Contributing](#contributing)
+        
+* [Tests](#tests)
+        
+* [Questions](#questions)
+        
+## Installation
+        
+To install necessary dependencies, run the following command:
+        
+\`\`\`
+${dependencies}
+\`\`\`
+        
+## Usage
+${about}
+        
+## License
+${licensetype}
+        
+## Contributing
+        
+${contributing}
+## Tests
+        
+To run tests, run the following command:
+        
+\`\`\`
+${tests}
+\`\`\`
+        
+## Questions
+![user profile image](${userImage})
+${userBlog}
+        
+`;
+
+}
